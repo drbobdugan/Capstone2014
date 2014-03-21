1b6c731d0fc71b91c05fc7b7f610948d22972082
bdugan <bdugan@stonehill.edu>
2014-03-21 18:34:42 +0000

    I am committing because this is so much fun!

---
+++ VolunteerTrack/src/main/java/stonehill/edu/VolunteerTrack/Kelseytest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package stonehill.edu.VolunteerTrack;
+
+import java.io.Serializable;
+
+/**
+ * @author ubuntu
+ *
+ */
+public class Kelseytest extends WebPage
+{
+private Label message;
+public Kelseytest()
+{
+message=new Label("Happy Friday Everyone!!");
+add(message);
+}
+}
