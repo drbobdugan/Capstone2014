54c77d8103af47acc80f285aa718dc7fbee18aaf
zbrown2 <zbrown2@students.stonehill.edu>
2014-03-27 23:17:41 +0000

    All my classes for week 11, need to fill in some functionality.

---
+++ VolunteerTrack/src/main/java/stonehill/edu/VolunteerTrack/PartnerEventView.java
@@ -0,0 +1,15 @@
+package stonehill.edu.VolunteerTrack;
+
+import java.io.Serializable;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+
+public class PartnerEventView extends WebPage 
+{
+	
+	public PartnerEventView()
+	{
+		
+	}
+}
