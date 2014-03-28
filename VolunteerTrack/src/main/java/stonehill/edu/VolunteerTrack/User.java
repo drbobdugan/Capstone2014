066af68dc33d38b087e1fa613bbda0915dfca709
ssifferlen <ssifferlen@students.stonehill.edu>
2014-03-28 15:48:29 +0000

    Added User Model

---
+++ VolunteerTrack/src/main/java/stonehill/edu/VolunteerTrack/User.java
@@ -0,0 +1,240 @@
+package stonehill.edu.VolunteerTrack;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+	private String email;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private String street;
+	private String city;
+	private String state;
+	private int zip;
+	private String phoneNumber;
+	private String partnerDescription;
+	private String volunteerDescription;
+	private int isPartner;
+	private int isCoordinator;
+	private int isVolunteer;
+	private int isApproved;
+	
+	// For serialization
+	static final long serialVersionUID = 9L;
+	
+	public User()
+	{
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	/**
+	 * @return the street
+	 */
+	public String getStreet() {
+		return street;
+	}
+
+	/**
+	 * @param street the street to set
+	 */
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	/**
+	 * @return the city
+	 */
+	public String getCity() {
+		return city;
+	}
+
+	/**
+	 * @param city the city to set
+	 */
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	/**
+	 * @return the state
+	 */
+	public String getState() {
+		return state;
+	}
+
+	/**
+	 * @param state the state to set
+	 */
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	/**
+	 * @return the zip
+	 */
+	public int getZip() {
+		return zip;
+	}
+
+	/**
+	 * @param zip the zip to set
+	 */
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+
+	/**
+	 * @return the phoneNumber
+	 */
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	/**
+	 * @param phoneNumber the phoneNumber to set
+	 */
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	/**
+	 * @return the partnerDescription
+	 */
+	public String getPartnerDescription() {
+		return partnerDescription;
+	}
+
+	/**
+	 * @param partnerDescription the partnerDescription to set
+	 */
+	public void setPartnerDescription(String partnerDescription) {
+		this.partnerDescription = partnerDescription;
+	}
+
+	/**
+	 * @return the volunteerDescription
+	 */
+	public String getVolunteerDescription() {
+		return volunteerDescription;
+	}
+
+	/**
+	 * @param volunteerDescription the volunteerDescription to set
+	 */
+	public void setVolunteerDescription(String volunteerDescription) {
+		this.volunteerDescription = volunteerDescription;
+	}
+
+	/**
+	 * @return the isPartner
+	 */
+	public int getIsPartner() {
+		return isPartner;
+	}
+
+	/**
+	 * @param isPartner the isPartner to set
+	 */
+	public void setIsPartner(int isPartner) {
+		this.isPartner = isPartner;
+	}
+
+	/**
+	 * @return the isCoordinator
+	 */
+	public int getIsCoordinator() {
+		return isCoordinator;
+	}
+
+	/**
+	 * @param isCoordinator the isCoordinator to set
+	 */
+	public void setIsCoordinator(int isCoordinator) {
+		this.isCoordinator = isCoordinator;
+	}
+
+	/**
+	 * @return the isVolunteer
+	 */
+	public int getIsVolunteer() {
+		return isVolunteer;
+	}
+
+	/**
+	 * @param isVolunteer the isVolunteer to set
+	 */
+	public void setIsVolunteer(int isVolunteer) {
+		this.isVolunteer = isVolunteer;
+	}
+
+	/**
+	 * @return the isApproved
+	 */
+	public int getIsApproved() {
+		return isApproved;
+	}
+
+	/**
+	 * @param isApproved the isApproved to set
+	 */
+	public void setIsApproved(int isApproved) {
+		this.isApproved = isApproved;
+	}
+	
+	
+}
