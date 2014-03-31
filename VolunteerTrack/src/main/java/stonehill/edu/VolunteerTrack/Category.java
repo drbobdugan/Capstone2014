f9c5e7544f6f55fd648cbce767deb833ba25135e
npiazza <ppiazza@students.stonehill.edu>
2014-03-31 04:03:45 +0000

    PartnerProfileView.html and Category Model

---
+++ VolunteerTrack/src/main/java/stonehill/edu/VolunteerTrack/Category.java
@@ -0,0 +1,19 @@
+package stonehill.edu.VolunteerTrack;
+
+public class Category {
+	
+	private String name;
+	
+	public Category(String n){
+		name=n;
+	}
+	
+	public void setName(String n){
+		name=n;
+	}
+	public String getName(){
+		return name;
+	}
+		
+
+}
