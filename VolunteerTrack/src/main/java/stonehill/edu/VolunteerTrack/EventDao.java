f971ebb69cc3058a493d0856a8877845691b3bd7
zbrown2 <zbrown2@students.stonehill.edu>
2014-04-01 19:55:40 +0000

    Changes to Event Dao and Partener Event View

--- VolunteerTrack/src/main/java/stonehill/edu/VolunteerTrack/EventDao.java
+++ VolunteerTrack/src/main/java/stonehill/edu/VolunteerTrack/EventDao.java
@@ -1,25 +1,143 @@
 package stonehill.edu.VolunteerTrack;
+
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Date;
 
-public class EventDao extends Dao {
+public class EventDao extends Dao{
 	public void delete(Object value) {
-		// TODO Auto-generated method stub
-		
+		Event event=(Event) value;
+		try{
+			//connect
+			connectToDatabase();
+			//SQL statement
+			Statement statement=connection.createStatement();
+			statement.executeQuery("DELETE FROM Event WHERE name = '"+  event.getName()  + "' AND " +
+			"location = '" + event.getLocation() +"' AND " +
+			"DateTime=to_date('"+new java.sql.Date(event.getDate().getTime())+"','yyyy-mm-dd')");
+			
+			statement.close();
+			disconnectFromDatabase();
+		}
+	    catch(Exception e){
+			e.printStackTrace();
+		}
 	}
 	public void insert(Object value) {
-		// TODO Auto-generated method stub
-		
+		Event event=(Event) value;
+		try{
+			//connect
+			connectToDatabase();
+			//SQL statement
+			Statement statement=connection.createStatement();
+			statement.executeQuery("INSERT INTO Event VALUES('"+
+		    event.getName()+"', "+
+            "to_date('"+new java.sql.Date(event.getDate().getTime())+"','yyyy-mm-dd'), "+
+		    "'"+event.getDescription()+"', "+
+		    "'"+event.getLocation()+"', 'none' ,"+ 
+		    "'"+event.getNumPositions()+"', "+
+		    "'"+event.getNumPositionsRemaining()+"')");
+			statement.close();
+			disconnectFromDatabase();
+		}
+	    catch(Exception e){
+			e.printStackTrace();
+		}
 	}
 	public ArrayList<Object> selectAll() {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Object> result=new ArrayList<Object>();
+		try{
+			//connect
+			connectToDatabase();
+			//SQL statement
+			Statement statement=connection.createStatement();
+			ResultSet resultSet=statement.executeQuery("SELECT * FROM Event");
+			//get tuples
+			while(resultSet.next()){
+				String name=resultSet.getString("Name");
+				Date date =resultSet.getDate("DateTime");
+				String description =resultSet.getString("Description");
+				String location =resultSet.getString("Location");
+				// TODO no sure how this will be handled yet
+				Skill [] skills= {new Skill(resultSet.getString("Interests"))};
+				int tp=resultSet.getInt("TotalPositions");
+				int tpr =resultSet.getInt("PositionsRemaining");
+				// TODO owners email
+				result.add( new Event ( "Owners  Email" , name, date,  description,  location ,  tp,  tpr, skills));
+			}
+			//clean up
+			resultSet.close();
+			statement.close();
+			disconnectFromDatabase();
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		return result;
 	}
 	public void update(Object value) {
-		// TODO Auto-generated method stub
-		
-	}
+		Event event=(Event) value;
+		try{
+			//connect
+			connectToDatabase();
+			//SQL statement
+			Statement statement=connection.createStatement();
 
+			statement.executeQuery("UPDATE Event SET "+
+			"Name='"+event.getName()+"', "+
+		    "Date='"+event.getDate()+"', "+
+		    "Description='"+event.getDescription()+"', "+
+		    "Location='"+event.getLocation()+"', "+
+		    "Interests='"+event.getSkills()+"', "+
+		    "TotalPositions='"+event.getNumPositions()+"', "+
+		    "PositionsRemaining='"+event.getNumPositionsRemaining()+"'WHERE "+   
+		    "Name='"+event.getName()+"'");
+		
+			statement.close();
+			disconnectFromDatabase();
+		}
+	    catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	public ArrayList<Object> getAllEventsByPartener(User user) {
+		ArrayList<Object> result=new ArrayList<Object>();
+		try{
+			//connect
+			connectToDatabase();
+			//SQL statement
+			Statement statement=connection.createStatement();
+			ResultSet resultSet=statement.executeQuery("SELECT * FROM Event, UserOwnsEvent WHERE UserEmail='"+user.getEmail()+"'"+ 
+			"AND Event.Name = UserOwnsEvent.EventName AND Event.DateTime = UserOwnsEvent.EventDateTime ");
+			
+			//get tuples
+			while(resultSet.next()){
+				String name=resultSet.getString("Name");
+				Date date =resultSet.getDate("Date");
+				String description =resultSet.getString("Description");
+				String location =resultSet.getString("Location");
+				int tp=resultSet.getInt("TotalPositions");
+				int tpr =resultSet.getInt("PositionsRemaining");
+				// TODO owners email
+				
+				ResultSet skillSet=statement.executeQuery("Select SkillName From Event, EventRequiresSkill Where Event.Name=EventRequiresSkill.EventName and Event.DateTime=EventRequiresSkill.EventDateTime and Event.name='"+name+"' and Event.DateTime=to_date('"+new java.sql.Date(date.getTime())+"','yyyy-mm-dd')");	
+				ArrayList<Skill> tempList = new ArrayList<Skill>(0);
+				while(skillSet.next()){
+					tempList.add(new Skill(skillSet.getString("skillName")));
+				}
+				result.add( new Event ( "Owners  Email" , name, date,  description,  location ,  tp,  tpr, (Skill[])tempList.toArray()));
+			}
+			
+			//clean up
+			resultSet.close();
+			statement.close();
+			disconnectFromDatabase();
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
 }
--- VolunteerTrack/src/main/java/stonehill/edu/VolunteerTrack/PartnerEventView.java
+++ VolunteerTrack/src/main/java/stonehill/edu/VolunteerTrack/PartnerEventView.java
@@ -14,7 +14,7 @@ import org.apache.wicket.markup.repeater.data.ListDataProvider;
 
 public class PartnerEventView extends VoltrackPage 
 {
-	private ArrayList<Event> futureEvents,pastEvents;  // list of events specific to the partner
+	private ArrayList futureEvents,pastEvents;  // list of events specific to the partner
 	
 	public PartnerEventView()
 	{
@@ -23,6 +23,21 @@ public class PartnerEventView extends VoltrackPage
 		// TODO get current user -> 
 		User theUser = new User("testEmail", "pass", "adsf", "asdf", "asdf", "aadsf", "asdf", "asdf", "af", "", "aasf", false, false, false, false);
 	    EventDao dao = new EventDao();
+	    
+	    
+	    Event e = new Event("joe@yahoo.com", "Event Name 1", new Date(), "des", "loc", 6, 4, new Skill[0]);
+		dao.insert(e);
+		e = new Event("zac@stonehill.edu", "Event Name 2", new Date(), "des", "loc", 3, 1, new Skill[0]);
+		dao.insert(e);
+	    e = new Event("billy@bob.com", "Event Name 3", new Date(), "des", "loc", 4, 2, new Skill[0]);
+		dao.insert(e);
+		
+		
+		futureEvents = dao.selectAll();
+		//System.out.println(ed.selectAll().size());
+		
+	//    ed.delete(e);
+	    
 	    
 	    /* TODO must test dao first 
 	    ArrayList<Object> events = (ArrayList<Object>) dao.getAllEventsByPartener(theUser);
@@ -38,11 +53,8 @@ public class PartnerEventView extends VoltrackPage
 	}  
 	
 	 private void populateTables() {
-		 ArrayList list = new ArrayList();
-		 Skill [] skills = null;
-		 list.add(new Event("email@place.com","Petting Puppies",  new Date(),"petting the puppoes", "Animal Shelter", 5, 3, skills));
-			     
-		final DataView dataView = new DataView("simple", new ListDataProvider(list)) {
+	
+		final DataView dataView = new DataView("simple", new ListDataProvider(futureEvents)) {
 			protected void populateItem(Item item) {
 				final Event event = (Event)item.getModelObject();
 				item.add(new Label("name", event.getName()));
@@ -53,9 +65,25 @@ public class PartnerEventView extends VoltrackPage
 			}
 		};
 		
-		dataView.setItemsPerPage(10);
+
+		final DataView dataView2 = new DataView("simple2", new ListDataProvider(pastEvents)) {
+			protected void populateItem(Item item) {
+				final Event event = (Event)item.getModelObject();
+				item.add(new Label("name2", event.getName()));
+				item.add(new Label("location2", event.getLocation()));
+				item.add(new Label("date2", event.getDate().toString()));
+				item.add(new Label("positions2", event.getNumPositions()));
+				item.add(new Label("available2", event.getNumPositionsRemaining()));
+			}
+		};
+		
+		dataView.setItemsPerPage(5);
 		add(dataView);
 		add(new PagingNavigator("navigator", dataView));
+		
+		dataView2.setItemsPerPage(5);
+		add(dataView2);
+		add(new PagingNavigator("navigator2", dataView2));
 		
 	}
 
