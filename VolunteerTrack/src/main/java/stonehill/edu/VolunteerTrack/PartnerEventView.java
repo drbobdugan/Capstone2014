9c9b9bf86fe049d4847641ab6e56242baa0b5cfc
zbrown2 <zbrown2@students.stonehill.edu>
2014-04-15 21:10:18 +0000

    Changes to use user stored in the custom sesson.

--- VolunteerTrack/src/main/java/stonehill/edu/VolunteerTrack/PartnerEventView.java
+++ VolunteerTrack/src/main/java/stonehill/edu/VolunteerTrack/PartnerEventView.java
@@ -9,10 +9,11 @@ import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.navigation.paging.PagingNavigator;
 import org.apache.wicket.markup.repeater.Item;
+import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.repeater.data.DataView;
 import org.apache.wicket.markup.repeater.data.ListDataProvider;
 
-public class PartnerEventView extends VolunteerTrackBaseView 
+public class PartnerEventView extends VolunteerTrackBaseView
 {
 	private ArrayList futureEvents,pastEvents;  // list of events specific to the partner
 	
@@ -20,10 +21,9 @@ public class PartnerEventView extends VolunteerTrackBaseView
 	{
 		futureEvents = new ArrayList<Event>(0);
 		pastEvents = new ArrayList<Event>(0);
-		// TODO get current user -> 
-		User user = new User("zbrown2@studesnt.stonehill.edu", "csrocks55", "Zac", "Brown", "320 Washington St.", "Easton", "Ma", "02357", "8025952931", "We help", "I help", true, false, false, false, "", "");
+		
 		EventDao dao = new EventDao();
-	    ArrayList<Object> events = dao.getAllEventsByOwner(user);
+	    ArrayList<Object> events = dao.getAllEventsByOwner(	 CustomSession.get().getUser());
 	    for(Object o: events){
 	    	if(((Event)o).getDate().after(new Date())){
 	    		futureEvents.add((Event)o);
@@ -38,6 +38,9 @@ public class PartnerEventView extends VolunteerTrackBaseView
 		final DataView dataView = new DataView("simple", new ListDataProvider(futureEvents)) {
 			protected void populateItem(Item item) {
 				final Event event = (Event)item.getModelObject();
+				item.add(new Link<Void>("delete"){ public void onClick(){ deleteEvent(0);}});
+				item.add(new Link<Void>("edit"){ public void onClick(){ editEvent(0);}});
+				item.add(new Link<Void>("view"){ public void onClick(){ viewEvent(0);}});
 				item.add(new Label("name", event.getName()));
 				item.add(new Label("location", event.getLocation()));
 				item.add(new Label("date", event.getDate().toString()));
@@ -48,6 +51,9 @@ public class PartnerEventView extends VolunteerTrackBaseView
 		final DataView dataView2 = new DataView("simple2", new ListDataProvider(pastEvents)) {
 			protected void populateItem(Item item) {
 				final Event event = (Event)item.getModelObject();
+				item.add(new Link<Void>("delete"){ public void onClick(){ deleteEvent(0);}});
+				item.add(new Link<Void>("edit"){ public void onClick(){ editEvent(0);}});
+				item.add(new Link<Void>("view"){ public void onClick(){ viewEvent(0);}});
 				item.add(new Label("name2", event.getName()));
 				item.add(new Label("location2", event.getLocation()));
 				item.add(new Label("date2", event.getDate().toString()));
@@ -55,6 +61,8 @@ public class PartnerEventView extends VolunteerTrackBaseView
 				item.add(new Label("available2", event.getNumPositionsRemaining()));
 			}
 		};
+		
+		add(new Link<Void>("new"){ public void onClick(){ createNewEvent();}});
 		
 		dataView.setItemsPerPage(5);
 		dataView2.setItemsPerPage(5);
@@ -67,7 +75,7 @@ public class PartnerEventView extends VolunteerTrackBaseView
 	}
 
 	// view an event
-    public void viewEvent(Event event){
+    public void viewEvent(int i){
    	 // TODO redireect 
     }
     
@@ -77,12 +85,12 @@ public class PartnerEventView extends VolunteerTrackBaseView
     }
     
     // edit an event
-    public void editEvent(Event event){
+    public void editEvent(int i){
    	 //TODO redirect
     }
     
     // delete an event
-    public void deleteEvent(Event event){
-   	 // TODO redirect
+    public void deleteEvent(int i){
+    	//TODO redirect
     }
 }
--- VolunteerTrack/src/main/java/stonehill/edu/VolunteerTrack/PartnerHomeView.java
+++ VolunteerTrack/src/main/java/stonehill/edu/VolunteerTrack/PartnerHomeView.java
@@ -1,5 +1,71 @@
 package stonehill.edu.VolunteerTrack;
 
-public class PartnerHomeView extends VolunteerTrackBaseView {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.navigation.paging.PagingNavigator;
+import org.apache.wicket.markup.repeater.Item;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.repeater.data.DataView;
+import org.apache.wicket.markup.repeater.data.ListDataProvider;
+
+public class PartnerHomeView extends VolunteerTrackBaseView{
+	private ArrayList futureEvents;  // list of events specific to the partner
+	private User currentUser;        // User that is acessing the page
+	public PartnerHomeView(){
+		futureEvents = new ArrayList<Event>(0);  // make emtpy array List to fill
+		currentUser = CustomSession.get().getUser();
+		EventDao dao = new 	EventDao();
+		
+	    ArrayList<Object> allEvents = dao.getAllEventsByOwner(currentUser);
+	    for(Object o: allEvents)
+	    	if(((Event)o).getDate().after(new Date()))  // if the date of the event is in the future add it to the list
+	    		futureEvents.add((Event)o);
+	    	
+	    populateTables();
+	}  
+	
+	 private void populateTables() {
+		final DataView dataView = new DataView("simple", new ListDataProvider(futureEvents)) {
+			protected void populateItem(Item item) {
+				final Event event = (Event)item.getModelObject();
+				item.add(new Link<Void>("delete"){ public void onClick(){ deleteEvent(0);}});
+				item.add(new Link<Void>("edit"){ public void onClick(){ editEvent(0);}});
+				item.add(new Link<Void>("view"){ public void onClick(){ viewEvent(0);}});
+				item.add(new Label("name", event.getName()));
+				item.add(new Label("location", event.getLocation()));
+				item.add(new Label("date", event.getDate().toString()));
+				item.add(new Label("positions", event.getNumPositions()));
+				item.add(new Label("available", event.getNumPositionsRemaining()));
+			}
+		};
+		
+		dataView.setItemsPerPage(5);
+		add(dataView);
+		add(new PagingNavigator("navigator", dataView));	
+	}
+
+	// view an event
+    public void viewEvent(int i){
+   	 // TODO redireect 
+    }
+    
+    // create a new event
+    public void createNewEvent(){
+   	 //TODO redirect
+    }
+    
+    // edit an event
+    public void editEvent(int i){
+   	 //TODO redirect
+    }
+    
+    // delete an event
+    public void deleteEvent(int i){
+    	//TODO redirect
+    }
 }
